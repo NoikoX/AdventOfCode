b3fa223c1fd3f4dda689d477439aa62359fa9eb6
Noe Lomidze <noikolomidze@gmail.com>
2023-12-05 00:33:31 +0400

    added day 4

---
+++ day4/Day4.py
@@ -0,0 +1,14 @@
+mapunia = {}
+for i, x in enumerate(open("text.txt", "r")):
+    if i not in mapunia:
+        mapunia[i] = 1
+    x = x.split(":")[1].strip()
+    a, b = [list(map(int, k.split())) for k in x.split(" | ")]
+    n = sum(p in a for p in b)
+
+    for k in range(i + 1, i + n + 1):
+        mapunia[k] = mapunia.get(k, 1) + mapunia[i]
+
+print(mapunia)
+print(sum(mapunia.values()))
+
